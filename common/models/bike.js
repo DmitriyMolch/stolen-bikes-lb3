ffd4d702b2ab9953431373be4041a799394dff52
Dmitriy1984Molchanov <dmitriy1984molchanov@gmail.com>
2019-03-25 20:20:56 +0300

    Implemented create bike validation.

--- common/models/bike.js
+++ common/models/bike.js
@@ -1,5 +1,9 @@
 'use strict';
 
 module.exports = function(Bike) {
-
+  Bike.beforeRemote('create', async() => {
+    Bike.validatesInclusionOf('found', {in: [false]});
+    Bike.validatesAbsenceOf('officerId');
+    Bike.validatesAbsenceOf('departmentId');
+  });
 };
