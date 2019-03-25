6bf188d74736042a7f28daf214a768e539b34b58
Dmitriy1984Molchanov <dmitriy1984molchanov@gmail.com>
2019-03-25 03:34:34 +0300

    Implemented schema update on migration, bikesProcessor, models relations.

---
+++ server/boot/02-bikesProcessor.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = async function(app) {
+  setInterval(async() => {
+    const bikes = await app.models.bike.find({
+      where: {officerId: {eq: null}},
+      order: 'date ASC',
+    });
+    const officers = await app.models.officer.getFree();
+    let promises = [];
+    bikes.some(bike => {
+      if (!officers.length) return true;
+      const index = Math.round(Math.random() * (officers.length - 1));
+      const officer = officers[index];
+      promises.push(bike.updateAttributes({
+        'officerId': officer.id,
+        'departmentId': officer.departmentId,
+      }));
+      officers.splice(index, 1);
+    });
+    await Promise.all(promises);
+    console.log('Bikes processed');
+  }, 1000);
+};
